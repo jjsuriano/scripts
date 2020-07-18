426633a3af29f6ea4e5b7e6c5721a3ded5e6a062
Julio Suriano <julios@vt.edu>
2020-07-18 17:09:46 -0600

    Added AWS

--- basics.py
+++ basics.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import boto3
 
 # SYS MODULE - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -27,10 +28,58 @@ print(os.getcwd())
 """
 Other useful methods:
 
-.remove("path of file") > removes a file
-.mkdir("path where dir will be created") > makes a directory in the path
-.rmdir("path of dir") > removes the dictionary
-.path.join("path1", "path2") > path2 will be joined to path1
-
+.remove("path of file") -> removes a file
+.mkdir("path where dir will be created") -> makes a directory in the path
+.rmdir("path of dir") -> removes the dictionary
+.path.join("path1", "path2") -> path2 will be joined to path1
+.path.split("path") -> splits the path
+.path.exists("path") -> determine if the path exists
 """
 print()
+
+# AWS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print("\nAWS", "="*57)
+
+"""
+Run "aws configure" and it will ask for the AWS Access ID and the AWS Secret
+Access Key
+"""
+
+# # EC2
+# ec2 = boto3.resource('ec2')
+
+# # CREATE AN INSTANCE
+# instance = ec2.create_instance(
+#     ImageId="",
+#     MinCount=1,
+#     MaxCount=1,
+#     InstanceType="",
+# )
+
+# print(instance[0].id)
+
+# # DELETE AN INSTANCE
+# # instance_id = ""
+# instance = ec2.Instance(instance_id)
+# response = instance.terminate()
+
+# print(response)
+
+# # S3
+# s3 = boto3.resource('s3')
+
+# # CREATE A BUCKET
+# name = ""
+# try:
+#     response = s3.create_bucket(
+#         Bucket=name,
+#         CreateBucketConfiguration={'LocationContraint': 'us-east-2'},
+#     )
+
+#     print(response)
+# except Exception as error:
+#     print(error)
+
+print()
+
